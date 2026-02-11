ba5b6718104c35e64bc350f8bf8757b8274b473d
Garchilla <dylan.king@ynov.com>
2026-02-11 11:23:50 +0100

    merge: merge into dev to make more simple

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -1,14 +1,1 @@
 package controller
-
-import (
-	"html/template"
-	"net/http"
-)
-
-func renderTemplate(w http.ResponseWriter, filename string, data map[string]string) {
-	tmpl := template.Must(template.ParseFiles("template/" + filename))
-	err := tmpl.Execute(w, data)
-	if err != nil {
-		return
-	}
-}
